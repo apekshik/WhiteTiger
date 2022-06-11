2f067e9b99e73d5bbee660995b098688debf3098
Apekshik Panigrahi <apekshik@gmail.com>
2022-06-11 11:47:32 +0530

    Initial Commit

---
+++ WhiteTiger/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WhiteTiger
+//
+//  Created by Apekshik Panigrahi on 6/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WhiteTiger/WhiteTigerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhiteTigerApp.swift
+//  WhiteTiger
+//
+//  Created by Apekshik Panigrahi on 6/11/22.
+//
+
+import SwiftUI
+
+@main
+struct WhiteTigerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
